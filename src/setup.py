9a8efb3a8d9ebda7244d405fbff1526bae45febb
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-09-09 16:01:46 -0300

    ordenando

--- src/setup.py
+++ src/setup.py
@@ -35,7 +35,7 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL License",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.8',
